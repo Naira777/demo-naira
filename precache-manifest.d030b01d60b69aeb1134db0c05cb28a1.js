bb10940966774a05aec8cf467f5d3533f2e3bdc5
Naira777 <sp_naira@yahoo.com>
2020-05-29 21:03:22 +0400

    Updates

---
+++ precache-manifest.d030b01d60b69aeb1134db0c05cb28a1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b345da2d39ff2069164060ab1c2ec628",
+    "url": "/demo-naira/index.html"
+  },
+  {
+    "revision": "820c66c64ad9b2a371b1",
+    "url": "/demo-naira/static/css/4.e1450926.chunk.css"
+  },
+  {
+    "revision": "ae658421d932208073c2",
+    "url": "/demo-naira/static/css/5.275413d5.chunk.css"
+  },
+  {
+    "revision": "c64cb5e187515b5d835e",
+    "url": "/demo-naira/static/css/6.00d7090a.chunk.css"
+  },
+  {
+    "revision": "3a826204bfd7dc1169a0",
+    "url": "/demo-naira/static/css/main.ac2cab33.chunk.css"
+  },
+  {
+    "revision": "07a6de248df8ec8453ca",
+    "url": "/demo-naira/static/js/0.8e0cd1d3.chunk.js"
+  },
+  {
+    "revision": "78648e15a546152d6c52",
+    "url": "/demo-naira/static/js/3.1f087ac2.chunk.js"
+  },
+  {
+    "revision": "33c7420501d7b0349de46a3a627e2c05",
+    "url": "/demo-naira/static/js/3.1f087ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820c66c64ad9b2a371b1",
+    "url": "/demo-naira/static/js/4.f420f742.chunk.js"
+  },
+  {
+    "revision": "ae658421d932208073c2",
+    "url": "/demo-naira/static/js/5.04cf4aa7.chunk.js"
+  },
+  {
+    "revision": "c64cb5e187515b5d835e",
+    "url": "/demo-naira/static/js/6.8c5626ee.chunk.js"
+  },
+  {
+    "revision": "3a826204bfd7dc1169a0",
+    "url": "/demo-naira/static/js/main.845d0429.chunk.js"
+  },
+  {
+    "revision": "b2710558d295877aadc0",
+    "url": "/demo-naira/static/js/runtime-main.deb555bf.js"
+  },
+  {
+    "revision": "f593b8260ef263cf2a8af2980fb2a6f0",
+    "url": "/demo-naira/static/media/preloader.f593b826.svg"
+  },
+  {
+    "revision": "6db09181ed93f7e8f2dac478623e3629",
+    "url": "/demo-naira/static/media/user.6db09181.png"
+  }
+]);
